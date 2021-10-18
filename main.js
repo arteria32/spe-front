48d709fe1232505533e8571674a5749054a3edac
arteria32 <denis.rjabko@mail.ru>
2021-10-18 08:05:02 +0300

    add circle menu

--- main.js
+++ main.js
@@ -153,6 +153,16 @@
         });
     });
 
+    var items = document.querySelectorAll('.circle a');
+    var k = 50;
+    for (var i = 0, l = items.length; i < l; i++) {
+        items[i].style.left = (50 - k * Math.cos(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+
+        items[i].style.top = (50 + k * Math.sin(-0.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+    }
+
+
+
 
 }());
 
